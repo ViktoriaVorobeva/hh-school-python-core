8874508300256da185acf5b4184e7e45536c453f
Viktoria <vorobeva.vik.99@mail.ru>
2024-01-14 18:38:30 +0300

    add homework

---
+++ hw/decorator.py
@@ -0,0 +1,11 @@
+import time
+
+def time_decorator(func):
+    def wrapper(*args, **kwargs):
+        print (f'Start for {func.__name__}')
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        print(f'Lead time: {(end_time - start_time)* 1000} mcs')
+        return result
+    return wrapper
--- hw/market.py
+++ hw/market.py
@@ -1,7 +1,24 @@
+from decorator import time_decorator
 class Market:
     def __init__(self, wines: list = None, beers: list = None) -> None:
+        self.wines = wines
+        self.beers = beers
+        self.drinks = self.get_dict_drinks(wines + beers)
         pass
 
+    @time_decorator
+    def get_dict_drinks(self, drinks: list) -> dict:
+        """
+        Метод получения словаря напитков
+        :param drinks:
+        :return: dict
+        """
+        dict = {}
+        for drink in drinks:
+            dict[drink.title] = drink.production_date
+        return dict
+    
+    @time_decorator
     def has_drink_with_title(self, title=None) -> bool:
         """
         Проверяет наличие напитка в магазине за О(1)
@@ -9,20 +26,31 @@ class Market:
         :param title:
         :return: True|False
         """
-        pass
+        return title in self.drinks
 
+    @time_decorator
     def get_drinks_sorted_by_title(self) -> list:
         """
         Метод получения списка напитков (вина и пива) отсортированных по title
 
         :return: list
         """
-        pass
+        all_drinks = list(self.drinks.keys())
+        return sorted(all_drinks)
 
+    @time_decorator
     def get_drinks_by_production_date(self, from_date=None, to_date=None) -> list:
         """
         Метод получения списка напитков в указанном диапазоне дат: с from_date по to_date
 
         :return: list
         """
-        pass
+        all_drinks = list(self.drinks.items())
+        filtered_drinks = []
+        for drink in all_drinks:
+            if from_date and drink[1] < from_date:
+                continue
+            if to_date and to_date < drink[1]:
+                continue
+            filtered_drinks.append(drink)    
+        return filtered_drinks
